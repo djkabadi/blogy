325d81206439e301d866dedddd06c5a7ff2749af
djkabadi <ihajikelvinogonda@gmail.com>
2022-11-02 17:53:28 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,28 @@
+// Filter Js
+$(document).ready(function () {
+  $(".filter-item").click(function () {
+    const value = $(this).attr("data-filter");
+    if (value == "all") {
+      $(".post-box").show("1000");
+    } else {
+      $(".post-box")
+        .not("." + value)
+        .hide("1000");
+      $(".post-box")
+        .filter("." + value)
+        .show("1000");
+    }
+  });
+
+  //   add active to btn
+  $(".filter-item").click(function () {
+    $(this).addClass("active-filter").siblings().removeClass("active-filter");
+  });
+});
+
+// header bg change on scroll
+let header = document.querySelector("header");
+
+window.addEventListener("scroll", () => {
+  header.classList.toggle("shadow", window.scrollY > 0);
+});
